9f76d2a3f15a991e8465fe30f1088d981db428a2
Tiago Alcantara <tiagofacampos@gmail.com>
2021-01-21 19:49:43 -0300

    Finished project

---
+++ src/br/com/alura/rh/model/Funcionario.java
@@ -0,0 +1,42 @@
+package br.com.alura.rh.model;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+
+import br.com.alura.rh.ValidacaoException;
+
+public class Funcionario {
+
+	private DadosPessoais dadosPessoais;
+	private LocalDate dataUltimoReajuste;
+
+	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
+		this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+	}
+
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
+	}
+
+	public void setDadosPessoais(DadosPessoais dadosPessoais) {
+		this.dadosPessoais = dadosPessoais;
+	}
+
+	public void atualizarSalario(BigDecimal novoSalario) {
+		this.dadosPessoais.setSalario(novoSalario);
+		this.dataUltimoReajuste = LocalDate.now();
+	}
+
+	public LocalDate getDataUltimoReajuste() {
+		return dataUltimoReajuste;
+	}
+
+	public void setDataUltimoReajuste(LocalDate dataUltimoReajuste) {
+		this.dataUltimoReajuste = dataUltimoReajuste;
+	}
+
+	public void promover(Cargo novoCargo) {
+		this.dadosPessoais.setCargo(novoCargo);
+	}
+}
